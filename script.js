a4d1145f6b187f6d827ebb3817fb556994265578
arthmrnd <theodinksson@gmail.com>
2021-01-04 09:31:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,74 @@
 const dinossaur = document.querySelector('.dino');
+const background = document.querySelector('.background');
+let jumping = false;
+let posi = 0;
+var theEnd = false;
 
+//handle key up
 function handleku(event) {
     if (event.keyCode === 32) {
-        toJump();
-        console.log("press space");
+        if (!jumping) {
+            toJump();
+        }
     }
 }
 
 function toJump() {
-    let posi = 0;
+    
+    jumping = true;
 
     let upGap = setInterval(() => {
         
-        if (posi >= 200) {
+        if (posi >= 170) {
             clearInterval(upGap);
-        }
+        
         //to down
         let downGap = setInterval(() => {
             if (posi <= 0) {
                 clearInterval(downGap)
+                jumping = false;
             }
             else {
-                posi -= 30;
+                posi -= 20;
                 dinossaur.style.bottom = posi + 'px';
             }
-        },30)
+        },20)
         //to up
-        posi += 30;
-        dinossaur.style.bottom = posi + 'px';
+        }else {
+            posi += 20;
+            dinossaur.style.bottom = posi + 'px';
+        }
 
-    },30 )
+    },20 )
 }
 
+function cactusGenesis() {
+    const catcus = document.createElement('div');
+    let posiCactus = 1000;
+    var ramdomizeCatus = Math.random() * 6000;
+
+    catcus.classList.add('cactus');
+    catcus.style.left = 1000 + 'px';
+    background.appendChild(catcus);
+
+    let leftTimer = setInterval (() => {
+        if (posiCactus < -60) {
+            // out of screen
+            clearInterval(leftTimer);
+            background.removeChild(catcus);
+        }
+        else if (posiCactus > 0 && posiCactus < 60 && posi < 60) {
+            clearInterval(leftTimer);
+            theEnd = true;
+            document.body.innerHTML = '<p class="gameOver">Game Over<\p>';
+        }
+        else {
+            posiCactus -= 10;
+            catcus.style.left = posiCactus + 'px';
+        }
+    }, 20);
+    setTimeout(cactusGenesis, ramdomizeCatus);
+}
+
+cactusGenesis();
 document.addEventListener('keyup', handleku);
